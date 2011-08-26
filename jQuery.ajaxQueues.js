b018785bf14aefd9c272d38fc72056262530b996
Michael Klein <mbklein@gmail.com>
2011-08-26 00:33:12 -0700

    Initial commit

---
+++ jQuery.ajaxQueues.js
@@ -0,0 +1,84 @@
+(function($) {
+  var queues = { 
+    default: newRequestContext();
+  };
+  
+  function newRequestContext(opts) {
+    opts = opts || {}
+    opts.maxRequests = opts.maxRequests || 5;
+    return({ pending: 0, requests: [], opts: opts });
+  }
+  
+  function sendNextRequest(queueName) {
+    var thisQueue = queues[queueName];
+    var context = thisQueue.requests.shift();
+    if (context) {
+      thisQueue.pending++;
+      $.ajax(context.ajaxOptions);
+    }
+  }
+  
+  function requestComplete(queueName) {
+    var thisQueue = queues[queueName];
+    thisQueue.pending--;
+    sendNextRequest(queueName);
+  }
+
+  function queueRequest(queueName, ajaxOptions, jqXHR) {
+    var thisQueue = queues[queueName];
+    thisQueue.requests.push({ ajaxOptions: ajaxOptions, xhr: jqXHR });
+    if (thisQueue.pending <= thisQueue.opts.maxRequests) {
+      sendNextRequest(queueName);
+    }
+  }
+  
+  function cancelRequest(queueName, jqXHR) {
+    var thisQueue = queues[queueName];
+    for (var i = 0; i < thisQueue.length; i++) {
+      if (thisQueue[i].xhr == jqXHR) {
+        thisQueue.splice(i,1).xhr.abort();
+      }
+    }
+  }
+  
+  $.extend({
+      ajaxQueue: function(queueName, opts) {
+        if (! queues.hasOwnProperty(queueName)) {
+            queues[queueName] = newRequestContext(opts);
+        } else {
+            throw "QueueError: queue '" + options.queue + "' is already defined."
+        }
+      },
+      clearQueue: function(queueName) {
+        var thisQueue = queues[queueName];
+        if (thisQueue) {
+          for (var i = 0; i < thisQueue.length; i++) {
+            var context = thisQueue[i];
+            cancelRequest(queueName, context.xhr);
+          }
+        }
+      }
+  });
+
+  $.ajaxTransport('queued',function(options, originalOptions, jqXHR) { 
+      var thisQueue = options.queue || 'default';
+      if (queues.hasOwnProperty(thisQueue)) {
+        return {
+          send: function(headers, completeCallback) {
+            var originalComplete = options.complete;
+            options.dataType = options.realDataType;
+            options.complete = function(xhr, status) {
+                originalComplete(xhr, status);
+                requestComplete(thisQueue);
+            }
+            queueRequest(thisQueue, options, jqXHR);
+          },
+          abort: function() {
+            cancelRequest(jqXHR);
+          }
+        }
+      } else {
+          throw "QueueError: queue '" + options.queue + "' is not defined."
+      }
+  });
+})(jQuery);
