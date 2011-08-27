d94b9f95058cca52e939b8a0784f51ac75cfd479
Michael Klein <mbklein@gmail.com>
2011-08-26 23:09:26 -0700

    Near-complete refactor - override internal $.ajax() to intercept and queue up anything with an ajaxQ property, using Deferreds and Promises to mimic the resulting XHR.

--- jquery.ajaxQ.js
+++ jquery.ajaxQ.js
@@ -9,89 +9,100 @@
  */
  
 (function($) {
-    var queues = { 
-      queue: newRequestContext()
-    };
-  
-    function newRequestContext(opts) {
-      opts = opts || {}
-      opts.maxRequests = opts.maxRequests || 5;
-      return({ pending: 0, requests: [], opts: opts });
-    }
-  
-    function sendNextRequest(queueName) {
-      var thisQueue = queues[queueName];
-      var context = thisQueue.requests.shift();
-      if (context) {
-        thisQueue.pending++;
-        if (context.ajaxOptions.dequeued) {
-          context.ajaxOptions.dequeued(context.xhr);
-        }
-        $.ajax(context.ajaxOptions);
+  var queues = { 
+    queue: newRequestQueue()
+  };
+
+  function newRequestQueue(opts) {
+    opts = opts || {}
+    opts.maxRequests = opts.maxRequests || 5;
+    return({ pending: 0, requests: [], opts: opts });
+  }
+
+  function sendNextRequest(queueName) {
+    var thisQueue = queues[queueName];
+    var context = thisQueue.requests.shift();
+    if (context) {
+      thisQueue.pending++;
+      if (context.ajaxOptions.dequeued) {
+        context.ajaxOptions.dequeued(context.xhr);
       }
+      
+      var jqxhr = $.ajax(context.ajaxOptions);
+      jqxhr.success(function(data, status, xhr) {
+        context.deferred.resolve(data, status, xhr);
+      });
+
+      jqxhr.error(function(jqXHR, status, error) {
+        context.deferred.reject(jqXHR, status, error);
+      });
+      
+      jqxhr.complete(function(jqXHR, status, error) {
+        requestComplete(queueName);
+      })
     }
-  
-    function requestComplete(queueName) {
-      var thisQueue = queues[queueName];
-      thisQueue.pending--;
+  }
+
+  function requestComplete(queueName) {
+    var thisQueue = queues[queueName];
+    thisQueue.pending--;
+    sendNextRequest(queueName);
+  }
+
+  function queueRequest(queueName, ajaxOptions, deferred) {
+    var thisQueue = queues[queueName];
+    thisQueue.requests.push({ ajaxOptions: ajaxOptions, deferred: deferred });
+    if (thisQueue.pending <= thisQueue.opts.maxRequests) {
       sendNextRequest(queueName);
     }
+  }
 
-    function queueRequest(queueName, ajaxOptions, jqXHR) {
-      var thisQueue = queues[queueName];
-      thisQueue.requests.push({ ajaxOptions: ajaxOptions, xhr: jqXHR });
-      if (thisQueue.pending <= thisQueue.opts.maxRequests) {
-        sendNextRequest(queueName);
+  function cancelRequest(queueName, deferred) {
+    var thisQueue = queues[queueName];
+    for (var i = 0; i < thisQueue.length; i++) {
+      if (thisQueue[i].deferred == deferred) {
+        thisQueue.splice(i,1).deferred.reject(deferred, 'aborted');
       }
     }
-  
-    function cancelRequest(queueName, jqXHR) {
-      var thisQueue = queues[queueName];
-      for (var i = 0; i < thisQueue.length; i++) {
-        if (thisQueue[i].xhr == jqXHR) {
-          thisQueue.splice(i,1).xhr.abort();
-        }
-      }
-    }
-  
-    $.extend({
-        ajaxQ: function(queueName, opts) {
-          if (! queues.hasOwnProperty(queueName)) {
-              queues[queueName] = newRequestContext(opts);
-          } else {
-              throw "QueueError: queue '" + options.queue + "' is already defined."
-          }
-        },
-        clearQ: function(queueName) {
-          var thisQueue = queues[queueName];
-          if (thisQueue) {
-            for (var i = 0; i < thisQueue.length; i++) {
-              var context = thisQueue[i];
-              cancelRequest(queueName, context.xhr);
-            }
-          }
-        }
-    });
+  }
 
-    $.ajaxTransport('ajaxQ',function(options, originalOptions, jqXHR) { 
-        var originalComplete = originalOptions.complete;
-        originalOptions.complete = function(xhr, status) {
-            originalComplete(xhr, status);
-            requestComplete(thisQueue);
-        }
-        originalOptions.dataType = originalOptions.realDataType;
-        var thisQueue = originalOptions.ajaxQ || 'queue';
-        if (queues.hasOwnProperty(thisQueue)) {
-          return {
-            send: function(headers, completeCallback) {
-              queueRequest(thisQueue, originalOptions, jqXHR);
-            },
-            abort: function() {
-              cancelRequest(jqXHR);
-            }
-          }
+  $.extend({
+      ajaxQ: function(queueName, opts) {
+        if (! queues.hasOwnProperty(queueName)) {
+            queues[queueName] = newRequestQueue(opts);
         } else {
-            throw "QueueError: queue '" + options.queue + "' is not defined."
+            throw "QueueError: queue '" + options.queue + "' is already defined."
         }
-    });
+      },
+      clearQ: function(queueName) {
+        var thisQueue = queues[queueName];
+        if (thisQueue) {
+          for (var i = 0; i < thisQueue.length; i++) {
+            var context = thisQueue[i];
+            cancelRequest(queueName, context.xhr);
+          }
+        }
+      }
+  });
+
+  var originalAjax = $.ajax;
+  $.ajax = function(opts) {
+    if (opts.ajaxQ) {
+      var thisQueue = opts.ajaxQ || 'queue';
+      if (queues.hasOwnProperty(thisQueue)) {
+        var deferred = $.Deferred();
+        var promise = deferred.promise();
+        promise.success = promise.done;
+        promise.error = promise.fail;
+        promise.complete = deferred.done;
+        delete opts['ajaxQ'];
+        queueRequest(thisQueue, opts, deferred);
+        return promise;
+      } else {
+        throw "QueueError: queue '" + options.queue + "' is not defined."
+      }
+    } else {
+      return originalAjax(opts);
+    }
+  }
 })(jQuery);
